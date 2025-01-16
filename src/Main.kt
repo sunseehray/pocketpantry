093766a4081b70713d0558f52c49e62c440f54d3
sunseehray <sabtirazona@byui.edu>
2025-01-16 12:58:42 +0800

    main menu

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, $name!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // app menu
+    fun DisplayMenu() {
+        println("-- Menu --")
+        println("1. Add stock")
+        println("2. Remove stock")
+        println("3. Create a new product")
+        println("4. See my inventory")
+        println("5. Exit")
+        println("What would you like to do? Enter the number only.")
     }
+
+    do {
+        DisplayMenu()
+        var choice = readLine()
+        println("You selected $choice")
+        when (choice) {
+            "1" -> println("Add stock")
+            "2" -> println("Remove stock")
+            "3" -> println("Create a new product")
+            "4" -> println("See my inventory")
+            else -> println("Error")
+        }
+    } while (choice != "5")
+
+
+
+
+
 }
